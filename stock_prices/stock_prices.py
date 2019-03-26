5f813d1f16e507d4164da104e84d7637fa6ddc74
Justin <jdlow2@gmail.com>
2019-03-26 14:58:16 -0400

    optimization for stock prices

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -5,12 +5,17 @@ import argparse
 
 def find_max_profit(prices):
     max_profit = None
+    lowest = None
     for i in range(len(prices) - 1):
         # find the highest sum for i: (the largest num in the array after i) - i
-        best = max(prices[i + 1 :]) - prices[i]
-        # replace max_profit if best is higher
-        if max_profit == None or best > max_profit:
-            max_profit = best
+        if (
+            lowest is None or prices[i] < lowest
+        ):  # no need to search if we've already calculated for a lower value
+            lowest = prices[i]
+            best = max(prices[i + 1 :]) - prices[i]
+            # replace max_profit if best is higher
+            if max_profit == None or best > max_profit:
+                max_profit = best
 
     return max_profit
 
